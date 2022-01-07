c4b801d3f7c1595874b577eb881e15f80bc296cd
dfgentil <gentildf@gmail.com>
2022-01-07 14:07:56 -0300

    terminado previamente os layouts de cadastro e login

--- app/src/main/java/com/example/seorganize/activity/CadastroActivity.java
+++ app/src/main/java/com/example/seorganize/activity/CadastroActivity.java
@@ -3,14 +3,29 @@ package com.example.seorganize.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.seorganize.R;
 
 public class CadastroActivity extends AppCompatActivity {
 
+    private EditText textNome, textEmail, textSenha;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        // Instanciar itens do layout
+        textNome = findViewById(R.id.textNome);
+        textEmail = findViewById(R.id.textEmail);
+        textSenha = findViewById(R.id.textPassword);
+
     }
+    public void botaoCadastrar(){
+
+    }
+
 }
