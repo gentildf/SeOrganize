c4cd3cf01c8bb7c146d242c3c13dd3a91ee71753
dfgentil <gentildf@gmail.com>
2022-01-07 11:01:21 -0300

    criado activity de login e cadastro e configurado botoes

--- app/src/main/java/com/example/seorganize/MainActivity.java
+++ app/src/main/java/com/example/seorganize/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.seorganize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.seorganize.activity.CadastroActivity;
+import com.example.seorganize.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,22 +24,27 @@ public class MainActivity extends IntroActivity {
         setButtonNextVisible(false);
 
         addSlide(new FragmentSlide.Builder()
-                .background(android.R.color.holo_green_dark)
-                .fragment(R.layout.slide_1)
-                .build()
-        );
-        addSlide(new FragmentSlide.Builder()
-                .background(android.R.color.holo_red_light)
-                .fragment(R.layout.slide_2)
-                .build()
-        );
-        addSlide(new FragmentSlide.Builder()
-                .background(android.R.color.holo_orange_dark)
-                .fragment(R.layout.slide_3)
+                .background(android.R.color.holo_blue_light)
+                .fragment(R.layout.boas_vindas)
                 .build()
         );
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.holo_blue_dark)
+                .fragment(R.layout.slide_1)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
+                .fragment(R.layout.slide_2)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_dark)
+                .fragment(R.layout.slide_3)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_light)
                 .fragment(R.layout.slide_4)
                 //.canGoForward(false) // para nao ir adiante o ultimo slide
                 .build()
@@ -46,6 +57,14 @@ public class MainActivity extends IntroActivity {
                 .build()
         );
 
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+
+    }
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
 
     }
 }
