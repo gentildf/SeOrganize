bf8abd5d40b1fbaf7d24d27265408cf2837400bb
dfgentil <gentildf@gmail.com>
2022-01-08 18:44:40 -0300

    limpado telas de fragments e organizado linhas do PrincipalActivity

--- app/src/main/java/com/example/seorganize/activity/DespesasActivity.java
+++ app/src/main/java/com/example/seorganize/activity/DespesasActivity.java
@@ -1,24 +1,21 @@
 package com.example.seorganize.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.seorganize.R;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.seorganize.R;
-
-public class ReceitasActivity extends AppCompatActivity {
+public class DespesasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_receitas);
+        setContentView(R.layout.activity_despesas);
     }
     public void voltarPrincipal(View view){
         startActivity(new Intent(this, PrincipalActivity.class));
-
     }
-
 
 }
